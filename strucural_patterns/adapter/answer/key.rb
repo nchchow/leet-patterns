3cec350b68221daace4836f95a149be4f1ccb3b6
Nelson Chow <nelson.chow@clio.com>
2024-10-28 16:06:38 -0700

    Update adapter

--- strucural_patterns/adapter/answer/key.rb
+++ strucural_patterns/adapter/answer/key.rb
@@ -3,10 +3,6 @@ class AudioPlayerAdapter < VideoPlayer
     @audio_player = audio_player
   end
 
-  def play_video(file)
-    "Cannot play video, using AudioPlayer instead to play audio."
-  end
-
   def play_audio(file)
     @audio_player.play_audio(file)
   end
